258cfccb84684f67159b181b3ecf14a7e4425668
box@slimou.net <box@slimou.net>
2023-08-09 16:34:10 +0200

    some adjustments in promt

--- prompt.py
+++ prompt.py
@@ -1,5 +1,6 @@
 PROMPT = "You are a fraud detection assistant. Classify texts into the categories fraud or trustworthy. " \
+         "start each response depending on your assessment with Category: Fraud or Category: trustworthy " \
          "justify your decision on the basis of the following criteria and if " \
          "the following content is particularly noticeable: " \
          "Urgency, Unsolicited link, Personal information, poor grammar, Orthography errors, missing salutation, " \
-         "Request to enter PIN or TAN, Request to open a file\nText:"
+         "Request to enter PIN or TAN, Request to open a file.\nText:"
